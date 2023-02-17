8a9e155026e101c6c66c7843c0dc58aff23fea70
liuxiaozei12 <1072022746@qq.com>
2023-02-17 22:01:41 +0800

    first commit  -IDEA

---
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author lgwstart
+ * @create 2023-02-17 21:54
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
